8e5838841295d05ba7d924cb7a8c8190d800d3da
voralv <vladimir.voral@socialbakers.com>
2020-06-27 15:34:41 +0200

    Added inputs for price, data, required, total fce

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
-var items = JSON.parse(localStorage.getItem("myObj")) || []
+function addExpense(e) {
+    var items = JSON.parse(localStorage.getItem("myObj")) || [];
 
-function giveMeData() {
-    boxvalue = document.getElementById('box').value;
-    items.push({name: boxvalue});
+    e.preventDefault();
 
+    let item = {
+        date: document.getElementById('date').value,
+        place: document.getElementById('place').value,
+        price: parseInt(document.getElementById('price').value),
+    };
+
+    items.push(item);
     localStorage.setItem('myObj', JSON.stringify(items));
+
+    updateTable()
+
 }
 
+function updateTable() {
+    var items = JSON.parse(localStorage.getItem("myObj")) || [];
+    let sum = items.map(item => item.price).reduce((prev, next) => prev + next, 0);
+
+    let getClass = document.querySelector(".test");
+    let total = document.getElementById("total");
+    getClass.innerHTML = items.map(item =>
+        `<tr>
+            <td>${item.date}</td>
+            <td>${item.place}</td>
+            <td>${item.price}</td>
+        </tr> 
+        `
+    ).join('');
+
+    total.innerHTML = `${sum}`;
+}
+
+// Remove current items from localStorage
 let getClass = document.querySelector(".test");
-
 let removeBtn = document.getElementById("remove");
-
-removeBtn.addEventListener('click', removeLocalStorage)
+removeBtn.addEventListener('click', removeLocalStorage);
 
 function removeLocalStorage () {
     localStorage.clear();
     getClass.remove()
 }
 
-getClass.innerHTML = items.map(item =>
-    `<tr>
-        <td>${item.name}</td>
-      </tr>             `
-).join('');
+let submitForm = document.getElementById("id1");
+submitForm.addEventListener('submit', addExpense);
+
+
+window.onload = function(){
+    updateTable()
+}
